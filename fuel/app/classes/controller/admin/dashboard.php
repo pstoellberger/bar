0cff3fe3de656fb464a5f1d652626cd88e0af8c4
pstoellberger <pstoellberger@gmail.com>
2015-03-06 14:15:15 +0100

    refactor dashboard

--- fuel/app/classes/controller/admin/dashboard.php
+++ fuel/app/classes/controller/admin/dashboard.php
@@ -8,39 +8,19 @@ class Controller_Admin_Dashboard extends Controller_Admin {
         
      	$this->template->title = "Items";
 
-       	$days=365;
-        $data=array();
-        $sales=$this->get_sales($days);
-        $paid=0;
-        $paidCount=0;
-        $unpaid=0;
-        $unpaidCount=0;
-        $total=0;
-        $totalCount=0;
+
+        $events=DB::query(" select year(now()) as year, month(now()) as month, count(*) as items, round(sum(price),2) as revenue, round(sum(profit),2) as profit from eventconsumptions where month(FROM_UNIXTIME(created_at)) = month(now()) and year(FROM_UNIXTIME(created_at)) = year(now())", DB::SELECT)->execute()->as_array();
+        $users=DB::query(" select year(now()) as year, month(now()) as month, count(*) as items, round(sum(price),2) as revenue, round(sum(profit),2) as profit from consumptions where month(FROM_UNIXTIME(created_at)) = month(now()) and year(FROM_UNIXTIME(created_at)) = year(now())", DB::SELECT)->execute()->as_array();
         
-        foreach($sales as $sale){
-        	if($sale->status!=1){
-        		$paid+=$sale->price;
-        		$paidCount++;
-        	}else{
-        		$unpaid+=$sale->price;
-        		$unpaidCount++;
-        	}
-        	$totalCount++;
-        	$total+=$sale->price;
-        }
-        
-       
-        $data['days']=$days;
-        $data['total']=$total;
-        $data['totalCount']=$totalCount;
-       	$data['paid']=$paid;
-       	$data['paidCount']=$paidCount;
-       	$data['unpaid']=$unpaid;
-       	$data['unpaidCount']=$unpaidCount;
-       	
-       	
-       	$data['users']=DB::count_records('users');
+        $data['month']=$events[0]['year'] . '-' . $events[0]['month'];
+
+        $data['useritems']=$users[0]['items'];
+        $data['usertotal']=$users[0]['revenue'];
+        $data['userprofit']=$users[0]['profit'];
+        $data['eventitems']=$events[0]['items'];
+        $data['eventtotal']=$events[0]['revenue'];
+        $data['eventprofit']=$events[0]['profit'];
+
        	
         $this->template->content=View::factory('admin/dashboard',$data);
 	}
